f93fc1b2e9eb91938bf3e7dddc99a0668bbb2f97
Sudhish Gopalakrishnan <sudhish.gk1@gmail.com>
2026-03-17 12:13:18 -0400

    Added crawlid turning animation

--- HollowKnight/StateMachines/CrawlidStateMachine.cs
+++ HollowKnight/StateMachines/CrawlidStateMachine.cs
@@ -1,3 +1,4 @@
+using System.Reflection;
 using HollowKnight.Factories;
 using HollowKnight.Shared;
 using Microsoft.Xna.Framework;
@@ -8,6 +9,9 @@ public class CrawlidStateMachine
 
     private const float PatrolSpeed = 60f; // TODO: change speed accordingly
     private Direction _movementDirection = Direction.Right;
+    private bool _isTurning = false;
+    private float _turnTimer = 0f;
+    private const float TurnDuration = 0.08f;
 
     public CrawlidStateMachine(Crawlid _enemy)
     {
@@ -18,15 +22,39 @@ public class CrawlidStateMachine
     {
         CurrentCrawlid.health--;
         if (CurrentCrawlid.health <= 0)
+        {
             CurrentCrawlid.alive = false;
-            
+            if (!CurrentCrawlid.IsGrounded)
+            {
+                CurrentCrawlid.state = 2;
+                CurrentCrawlid.CrawlidSprite = SpriteFactory.Instance.CreateCrawlidDeathAirSprite(CurrentCrawlid.position);
+            }
+            else
+            {
+                CurrentCrawlid.state = 3;
+                CurrentCrawlid.CrawlidSprite = SpriteFactory.Instance.CreateCrawlidDeathLandSprite(CurrentCrawlid.position);
+            }
+        }
     }
 
     public void Update(GameTime _gameTime)
     {
         if (!CurrentCrawlid.alive || CurrentCrawlid.IsDamaged) return;
-        // TODO: Add wall/edge detection to trigger Turn state while patrolling
         float elapsedTime = (float)_gameTime.ElapsedGameTime.TotalSeconds;
+
+        if (_isTurning)
+        {
+            _turnTimer += elapsedTime;
+            if (_turnTimer >= TurnDuration)
+            {
+                _isTurning = false;
+                _turnTimer = 0f;
+                CurrentCrawlid.state = 0;
+                CurrentCrawlid.CrawlidSprite = SpriteFactory.Instance.CreateCrawlidIdleSprite(CurrentCrawlid.position);
+            }
+            CurrentCrawlid.CrawlidSprite.SetPosition(CurrentCrawlid.position);
+            return;
+        }
 
         CurrentCrawlid.position.X += (_movementDirection == Direction.Right ? PatrolSpeed : -PatrolSpeed) * elapsedTime;
         float spriteWidth = CurrentCrawlid.CrawlidSprite.GetSize().X;
@@ -36,15 +64,25 @@ public class CrawlidStateMachine
             CurrentCrawlid.position.X = 1280 - spriteWidth;
             _movementDirection = Direction.Left;
             CurrentCrawlid.facingDirection = Direction.Left;
+            CrawlidTurn();
         }
         else if (CurrentCrawlid.position.X <= 0)
         {
             CurrentCrawlid.position.X = 0;
             _movementDirection = Direction.Right;
             CurrentCrawlid.facingDirection = Direction.Right;
+            CrawlidTurn();
         }
         
         CurrentCrawlid.CrawlidSprite.SetPosition(CurrentCrawlid.position);
+    }
+
+    private void CrawlidTurn()
+    {
+        _isTurning = true;
+        _turnTimer = 0f;
+        CurrentCrawlid.state = 1;
+        CurrentCrawlid.CrawlidSprite = SpriteFactory.Instance.CreateCrawlidTurnSprite(CurrentCrawlid.position);
     }
 
     public string GetStateName()
