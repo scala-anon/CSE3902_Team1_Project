163736ee8689906f7a71f40ffa54a4ada62ad124
Sudhish Gopalakrishnan <sudhish.gk1@gmail.com>
2026-04-20 19:09:11 -0400

    doors are breakable

--- HollowKnight/Environment/Door.cs
+++ HollowKnight/Environment/Door.cs
@@ -1,15 +1,24 @@
 using HollowKnight.Factories;
 using HollowKnight.Interfaces;
+using HollowKnight.Player;
+using HollowKnight.Shared;
 using Microsoft.Xna.Framework;
 
 namespace HollowKnight.Environment
 {
     public enum DoorState { Full, Half, Broken }
 
-    public class Door : SizedEnvironmentObject
+    public class Door : SizedEnvironmentObject, IInteractable, IBreakable
     {
-        private readonly int variant;
+        private int _hitCount = 0;
+        private bool _broken = false;
+        private const int HitsToBreak = 3;
+
         public override string Label => "Door";
+        public override bool IsActive => !_broken;
+
+        public int Health => HitsToBreak - _hitCount;
+        public InteractionType InteractionType => InteractionType.SwordHit;
 
         public Door(Vector2 position, int hitWidth, int hitHeight, int hitOffsetY = 0)
         {
@@ -23,9 +32,32 @@ namespace HollowKnight.Environment
 
         public void Break()
         {
-            sprite = variant == 1
-                ? SpriteFactory.Instance.CreatePlant1ChoppedSprite(position)
-                : SpriteFactory.Instance.CreatePlant2ChoppedSprite(position);
+            if (_broken) return;
+            _broken = true;
+            // TODO: swap to broken door sprite once art asset exists; Door_1 used as placeholder
+            sprite = SpriteFactory.Instance.CreateDoorHitSprite(position);
+            DebugLogger.LogObject($"Door broken: {Label}");
+        }
+
+        public Rectangle[] GetInteractionBounds()
+        {
+            int expand = CollisionConstants.BreakableWallInteractionExpand;
+            Rectangle b = GetBounds()[0];
+            return new[]
+            {
+                new Rectangle(b.X - expand, b.Y - expand, b.Width + expand * 2, b.Height + expand * 2)
+            };
+        }
+
+        public bool IsInteractable(TheKnight knight) => !_broken;
+
+        public void OnInteract(TheKnight knight)
+        {
+            if (_broken) return;
+            _hitCount++;
+            DebugLogger.LogObject($"Door hit: {Label} ({_hitCount}/{HitsToBreak})");
+            if (_hitCount >= HitsToBreak)
+                Break();
         }
     }
 }
