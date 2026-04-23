48c447319f948e85869f1ed3cc4c08eaebc5265b
Sudhish Gopalakrishnan <sudhish.gk1@gmail.com>
2026-04-23 19:44:45 -0400

    updated knight slash for room reactions

--- HollowKnight/Shared/CollisionConstants.cs
+++ HollowKnight/Shared/CollisionConstants.cs
@@ -29,10 +29,11 @@ namespace HollowKnight.Shared
         public const int KnightHitboxHeightShrink = 10;
 
         // Sword Hitbox Dimensions
-        public const int SideSlashWidth = 160;
-        public const int SideSlashHeight = 110;
-        public const int UpSlashWidth = 90;
-        public const int UpSlashHeight = 120;
+        public const int SideSlashWidth = 193;
+        public const int SideSlashHeight = 83;
+        public const int SideSlashOffsetY = -15;
+        public const int UpSlashWidth = 143;
+        public const int UpSlashHeight = 146;
         public const int DownSlashWidth = 90;
         public const int DownSlashHeight = 120;
 
